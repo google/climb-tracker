5afe181c9f5762e6a491e5aed62861dd8c0edf92
Steren Giannini <steren@google.com>
2015-06-26 12:09:34 +0200

    display an action bar and up button in the Settings activity

--- mobile/src/main/java/fr/steren/climbtracker/SettingsActivity.java
+++ mobile/src/main/java/fr/steren/climbtracker/SettingsActivity.java
@@ -1,16 +1,31 @@
 package fr.steren.climbtracker;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Show the Up button in the action bar.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            navigateUpTo(new Intent(this, ClimbTracker.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
